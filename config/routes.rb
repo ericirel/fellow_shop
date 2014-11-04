80a1c47ec4a24a7516a674e0ad493235fa0b1989
Benjamin Loustau <benloustau@gmail.com>
2014-11-04 13:56:21 -0500

    added controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
   devise_for :users, :controllers => { :registrations => "registrations" }
+                  , :controllers => {:sessions => "sessions"}
 
   resources :users
-  
+
   root 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

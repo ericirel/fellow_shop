1fc2a774130ea191c804e9edd548c8693b1e4339
Angas <angasreid@gmail.com>
2014-11-04 13:02:57 -0500

    Add gibbon gem and Mailchimp API

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 class UsersController < ApplicationController
 end
+
+def subscribe
+  @user = User.find(params[:id])
+  @user.subscribe_to_mailchimp(true)
+  # redirect to some other url
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,28 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+	  def subscribe_to_mailchimp(optin = false)
+	    response = $mailchimp.lists.subscribe({
+	       id: 'Angas Test',
+	       email: {email: self.email},
+	       merge_vars: { fname: self.name1, lname: self.name2 },
+	       double_optin: optin,
+	    })
+	end
+
+	def unsubscribe_from_mailchimp(notify = false)
+	    response = $mailchimp.lists.unsubscribe({
+	       id: 'Angas Test',
+	       email: {email: self.email},
+	       :delete_member => false, 
+	       :send_notify => notify
+	    })
+	end
+
+
+	private
+    def subscribe_to_list
+        Resque.enqueue(MailchimpSubscriber, self.id)
+    end
 end

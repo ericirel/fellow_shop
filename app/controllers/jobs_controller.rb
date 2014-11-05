b81efa5901985c126423bc4b36c98f720a17d248
eric-irel <eric.herskovic@gmail.com>
2014-11-05 16:01:27 -0500

    fixed merge conflict

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,10 +2,13 @@ class JobsController < ApplicationController
 
   def index
     @jobs = Job.all
+    @jobs = Job.search(params[:search])
+    @tags = Tag.search(params[:search])
   end
 
   def show
     @job = Job.find(params[:id])
+    @tag = Tag.find(params[:id])
   end
 
   def new
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -5,4 +5,19 @@ class Tag < ActiveRecord::Base
   def to_s
     name
   end
+
+  def tag_list=(tags_string)
+    tag_names = tags_string.split(",").collect{|s| s.strip.downcase}.uniq
+    new_or_found_tags = tag_names.collect { |name| Tag.find_or_create_by(name: name) }
+    self.tags = new_or_found_tags
+  end
+
+  def self.search(search)
+    if search
+      #find(:all, :conditions => ['title LIKE ?', "#{search}"])
+      where('name LIKE :search', search: "%#{search}%")
+    else
+      self.all
+    end
+  end
 end

7092fbf94e10e2834b1c03fed165c8c21cbf9b89
Benjamin Loustau <benloustau@gmail.com>
2014-11-06 11:00:20 -0500

    added github_link

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141105131321) do
+ActiveRecord::Schema.define(version: 20141106155743) do
 
   create_table "comments", force: true do |t|
     t.text     "body"
@@ -82,6 +82,7 @@ ActiveRecord::Schema.define(version: 20141105131321) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.string   "github_link"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true

553b540642552c8a342d2266982427c42f861524
Angas <angasreid@gmail.com>
2014-11-06 20:40:28 -0500

    Merge conflict resolution

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,7 +33,7 @@ class User < ActiveRecord::Base
     end
 
 
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>", :small => "50x50>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>", :small => "50x50>" }, :default_url => "/images/:style/placeholder.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/       
 
 
